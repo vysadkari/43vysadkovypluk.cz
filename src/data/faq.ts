468f1266585113e2471f03c4bf1be664c0a767ab
Pavel Štěrba <email@pavelsterba.com>
2023-12-16 09:48:55 +0100

    #12 Přidána sekce Výběrové řízení

---
+++ src/data/faq.ts
@@ -0,0 +1,47 @@
+type FAQ = {
+  question: string;
+  answer: string;
+};
+
+const faq: FAQ[] = [
+  {
+    question: "Jak se mám na výběrové řízení nejlépe připravit?",
+    answer:
+      "Samozřejmostí je být fyzicky na velmi dobré úrovni – pomůže ti to i psychicky. Výběrové řízení v dlouhodobém průměru nedokončí polovina uchazečů. Ti zpravidla podcenili přípravu.",
+  },
+  {
+    question: "Mohu mít na výběrové řízení vlastní vybavení?",
+    answer:
+      "Ano, na výběrové řízení si klidně vezmi svoje pevné černé boty, které se běžně v AČR nosí, stejně tak batoh. Na úvodní fyzické přezkoušení můžeš mít vlastní cvičební úbor.",
+  },
+  {
+    question:
+      "Jsou během výběrového řízení zajištěny potraviny a doplňky stravy?",
+    answer:
+      "Ne, stravu si každý účastník musí zajistit sám. Běžně dostupné energetické nápoje nebo sportovní doplňky stravy jsou povoleny. Zakázán je alkohol, tabákové výrobky a léky bez vědomí lékaře nebo zdravotního instruktora.",
+  },
+  {
+    question:
+      "Jaké potvrzení od lékaře musím mít? Stačí klasické potvrzení z posádkové ošetřovny?",
+    answer:
+      "Ano, stačí potvrzení, které ti vystaví tvůj posádkový doktor. Jen mu řekni, že se chceš zúčastnit výběrového řízení k výsadkářům.",
+  },
+  {
+    question:
+      "Je zajištěno závaží na zrychlený přesun a pochod? Co mám použít?",
+    answer:
+      "Ne, zátěž do batohu si každý účastník zajišťuje sám. Fantazii se meze nekladou, nejčastější jsou pytle s pískem, kotouče z posilovny nebo bedýnky s municí naplněné štěrkem či pískem. Předem si ale vyzkoušej, jak se ti tebou zvolené závaží bude chovat v batohu a jak se ti ponese. U pochodu se hlídá váha, se kterou dojdeš do cíle, ne se kterou se vydáš na cestu.",
+  },
+  {
+    question: "Mám drobné zdravotní potíže. Můžu se k vám přihlásit?",
+    answer:
+      "Tvůj zdravotní stav nehodnotíme my, ale vojenská nemocnice, do které půjdeš na vstupní prohlídku před vstupem do armády. Pro nás je důležité, aby ve tvém zdravotním vysvědčení stálo: „A - schopen jako výsadkář“. Výběrové řízení můžeš absolvovat i bez něj, ale musíš si ho nejpozději do 2 let (taková je platnost výsledku výběrového řízení) obstarat. Bez něj tě bohužel přijmout nemůžeme.",
+  },
+  {
+    question: "Co se stane, pokud ani napotřetí neuspěji ve výběrovém řízení?",
+    answer:
+      "Pokud ani na třetí pokus neuspěješ ve výběrovém řízení, budeš přeložen na jiný útvar dle potřeb AČR.",
+  },
+];
+
+export default faq;
